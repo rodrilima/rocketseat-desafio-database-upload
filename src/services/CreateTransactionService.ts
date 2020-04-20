c93ab92694e619aeb33e96ba446dca9411352d0d
rodrilima <rodelima20@gmail.com>
2020-04-20 11:34:27 -0300

    chore: create route and service create transactions

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,39 @@
+import { getRepository ,getCustomRepository } from 'typeorm'
+
 // import AppError from '../errors/AppError';
 
 import Transaction from '../models/Transaction';
+import Category from '../models/Category';
+import TransactionsRepository from '../repositories/TransactionsRepository'
+
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+  category: string;
+}
 
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({ title, value, type, category }: Request): Promise<Transaction> {
+    const categoryRepository = getRepository(Category);
+
+    const categoryFound = await categoryRepository.findOne({ title: category })
+
+    const transactionCategory = categoryFound
+    || await categoryRepository.create({ title: category })
+
+    if(!categoryFound) await categoryRepository.save(transactionCategory)
+
+    const transactionsRepository = getCustomRepository(TransactionsRepository);
+    const transaction = await transactionsRepository.create({
+      title,
+      value,
+      type,
+      category_id: transactionCategory.id
+    });
+
+    await transactionsRepository.save(transaction);
+    return transaction;
   }
 }
 
