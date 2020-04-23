0d9357bb8ea0ec56fc12a8a70b847c123dc6e6d2
rodrilima <rodelima20@gmail.com>
2020-04-23 07:20:47 -0300

    chore: add cors and eager loading

--- src/models/Category.ts
+++ src/models/Category.ts
@@ -2,8 +2,11 @@ import {
   Entity,
   Column,
   PrimaryGeneratedColumn,
-  CreateDateColumn
+  CreateDateColumn,
+  OneToMany
 } from 'typeorm';
+
+import Transaction from './Transaction';
 
 @Entity("categories")
 class Category {
@@ -13,6 +16,9 @@ class Category {
 
   @Column()
   title: string;
+
+  @OneToMany(() => Transaction, transaction => transaction.category)
+  transaction: Transaction;
 
   @CreateDateColumn()
   created_at: Date;
